77023b92de50509fbd1a7e277af7ed3be5e576d9
krishraj7312-gif <krishraj7312@gmail.com>
2026-04-04 21:55:12 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (81.06%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,25 +3,20 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    // start from last
-    let n = s.length-1;
-
-    while(n>=0){  // condition is that i will run until there is one space to stop at -1
-        if(s[n] === " "){
-            n--; // removing all the spaces
-        }else{
-            break;// is space end means alohabet starrt then we wil break it
-        }
-    }
-    // now we reach at last alphabet
+    // approach 3
+    let n = s.length-1
     let count = 0;
     while(n>=0){
-        if(s[n] != " "){ // countning until i reach the last character
-            --n;
+        if(s[n] != " "){
+            
             ++count;
-        }else{
-            break;
+
         }
+        else if(count>0){
+            break;
+
+        }
+        --n;
     }
     return count;
 };
