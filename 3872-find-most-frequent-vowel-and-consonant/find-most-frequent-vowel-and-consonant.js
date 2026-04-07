6c41a7fb87585372c4572574b3c7f91b539945e2
krishraj7312-gif <krishraj7312@gmail.com>
2026-04-07 22:43:52 +0530

    Time: 10 ms (32.89%) | Memory: 57.2 MB (53.31%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -3,25 +3,21 @@
  * @return {number}
  */
 var maxFreqSum = function(s) {
-    const freq = new Array(26).fill(0);
-    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
-
-    for (const ch of s) {
-        freq[ch.charCodeAt(0) - 97]++;
+   let map = {};
+   for(let i =0;i<s.length;i++){
+    map[s[i]] = !map[s[i]] ? 1 : ++map[s[i]];
+   }
+   let vowels = ['a','e','i','o','u']
+   let maxVowel = 0;
+   let maxConsonant =0;
+   let mapKeys = Object.keys(map);
+   for(let i = 0; i< mapKeys.length; i++){
+    if(vowels.includes(mapKeys[i])){
+        maxVowel = Math.max(maxVowel, map[mapKeys[i]])
     }
-
-    let maxVowel = 0;
-    let maxConsonant = 0;
-
-    for (let i = 0; i < 26; i++) {
-        const ch = String.fromCharCode(i + 97);
-
-        if (vowels.has(ch)) {
-            maxVowel = Math.max(maxVowel, freq[i]);
-        } else {
-            maxConsonant = Math.max(maxConsonant, freq[i]);
-        }
+    else {
+        maxConsonant = Math.max(maxConsonant, map[mapKeys[i]])
     }
-
-    return maxVowel + maxConsonant;
+   }
+   return maxConsonant + maxVowel;
 };
