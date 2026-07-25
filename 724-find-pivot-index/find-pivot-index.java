2672d12038f0720c15b4e5bc9e6a91a06bfee95e
krishraj7312-gif <krishraj7312@gmail.com>
2026-07-26 02:56:38 +0530

    Time: 1 ms (97.84%) | Memory: 47.3 MB (60.37%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int rightsum = 0;
+        for(int i =0; i<nums.length; i++){
+            rightsum += nums[i];
+        }
+        int leftsum = 0;
+        for(int i =0; i < nums.length; i++){
+            if(leftsum == rightsum- nums[i]){
+                return i;
+            }
+            leftsum+= nums[i];
+            rightsum-=nums[i];
+        }
+        return -1;
+    }
+}
