3ccddd4766e0b9835ec4dae20221e2baca04b525
krishraj7312-gif <krishraj7312@gmail.com>
2026-07-30 07:57:56 +0530

    Time: 2 ms (70.82%) | Memory: 55.6 MB (46.52%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,21 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int count = 0;
+        int candi = 0;
 
-        // Populate the frequency map
-        for (int i = 0; i < nums.length; i++) {
-            // getOrDefault returns the current count, or 0 if it's the first time seeing it
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-
-        // Find the element with a frequency > n / 2
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > nums.length / 2) {
-                return entry.getKey();
+        for ( int num : nums){
+            if(count == 0){
+                candi  =  num;
+            }
+            if (num == candi ){
+                count ++;
+            }
+            else{
+                count --;
             }
         }
-        
-        return -1; // Standard fallback if no majority element exists
+        return candi ;
+
     }
 }
